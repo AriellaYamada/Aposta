5fd960809aef0cff21c3ccfb086b653d210e41e1
AriellaYamada <ariella.y.b@hotmail.com>
2015-03-28 10:37:06 -0300

    Primeita estratégia

---
+++ CompetitorLegal.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.usp.icmc.ssc01032015;
+
+public class CompetitorLegal implements Competitor {
+
+   private double total_amount;
+   private double donation;
+   
+   public CompetitorLegal ()
+   {
+       total_amount = 10;
+       donation = 5;
+   }
+   
+   public double declareDonationTo(Competitor c) {
+       total_amount-=5;
+       return 5;
+       
+   }
+   
+   public void informDonationFrom(Competitor c, double donation) {
+       
+       
+   }
+   
+   public void addCash (double amount) {
+       
+   }
+   
+   public double getTotalCash() {
+       return total_amount;
+   }
+   
+   
+    
+}
