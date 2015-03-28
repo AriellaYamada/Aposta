d5eb1bdb2732ceed9b7f9a881bcbcdcb26e2928b
AriellaYamada <ariella.y.b@hotmail.com>
2015-03-28 09:49:41 -0300

    Compedidor 1

---
+++ Competitor1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.usp.icmc.ssc01032015;
+
+public class Competitor1 implements Competitor {
+
+   private int total_amount;
+   
+   
+    
+}
